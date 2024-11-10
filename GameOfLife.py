4bc32f4d944648d14ed41b8de0c3cae192f886e4
maticjaksic <127771687+maticjaksic@users.noreply.github.com>
2024-11-10 22:20:06 +0100

    Update GameOfLife.py
    + not if

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,12 +1,15 @@
-import pygame 
+import pygame
+import random
+
 clock = pygame.time.Clock()
-fps = 8
+fps = 480
 
 running = True
 
 aliveRGB = (255,255,255)
 deadRGB = (20,20,20)
 #####################################################################################################################
+
 def setupScreen(size):
     global screen
     global cellSize
@@ -20,7 +23,7 @@ def setupArray(width, height):
     global arrayHeight
     arrayWidth = width
     arrayHeight = height
-    boardArray= [ [0]*width for i in range(height)]
+    boardArray= [ [0]*width for i in range(height)] 
 
 def drawCell(surface,color,left,top,sideLength):
     pygame.draw.rect(surface, color, pygame.Rect(left,top,sideLength,sideLength))
@@ -33,15 +36,32 @@ def drawArray():
             else:
                 drawCell(screen,deadRGB,x*cellSize+1,y*cellSize+1,cellSize-2)
 
-def updateBoardArray(array):
-    newBoardArray = [ [0]*arrayWidth for i in range(arrayHeight)]
-    for x in range(arrayWidth):
-        for y in range(arrayHeight):
-            if (x == 0 or y >= arrayWidth-1 or y == 0 or x >= arrayHeight-1) == False:
+# def updateBoardArray(array):
+#     newBoardArray = [ [0]*arrayWidth for i in range(arrayHeight)]
+#     for x in range(arrayWidth):
+#         for y in range(arrayHeight):
+#             if (x == 0 or y >= arrayWidth-1 or y == 0 or x >= arrayHeight-1) == False:
                 
-                newBoardArray[x][y] = getState(x,y)
-    array = []
-    array = newBoardArray
+#                 newBoardArray[x][y] = getState(x,y)
+#     if array == newBoardArray:
+#         global simulating
+#         simulating = False
+#     array = []
+#     array = newBoardArray
+#     return array
+
+def updateBoardArray(array):
+    oldArray = [0]*arrayWidth
+    for x in range(arrayWidth):   
+        firstLineArray = [0]*arrayWidth
+        for y in range(arrayHeight):
+            if (y >= arrayWidth-1 or x >= arrayHeight-1) == False:               
+                firstLineArray[y] = getState(x,y)
+
+        if x != 0:      
+            array[x-1] = oldArray
+        oldArray = firstLineArray
+
     return array
                 
             
@@ -51,32 +71,38 @@ def getState(x,y):
 
     counter = 0
 
-    if boardArray[x-1][y-1] == 1:
-        counter += 1
-    if boardArray[x][y-1] == 1:
-        counter += 1
-    if boardArray[x+1][y-1] == 1:
-        counter += 1
-    if boardArray[x-1][y] == 1:
-        counter += 1
-    if boardArray[x+1][y] == 1:
-        counter += 1
-    if boardArray[x-1][y+1] == 1:
-        counter += 1
-    if boardArray[x][y+1] == 1:
-        counter += 1
-    if boardArray[x+1][y+1] == 1:
-        counter += 1
+    # if boardArray[x-1][y-1] == 1:
+    #     counter += 1
+    # if boardArray[x][y-1] == 1:
+    #     counter += 1
+    # if boardArray[x+1][y-1] == 1:
+    #     counter += 1
+    # if boardArray[x-1][y] == 1:
+    #     counter += 1
+    # if boardArray[x+1][y] == 1:
+    #     counter += 1
+    # if boardArray[x-1][y+1] == 1:
+    #     counter += 1
+    # if boardArray[x][y+1] == 1:
+    #     counter += 1
+    # if boardArray[x+1][y+1] == 1:
+    #     counter += 1
 
-    
-    if state == 1 and (counter == 2 or counter == 3):
+    counter = boardArray[x-1][y-1] + boardArray[x][y-1] + boardArray[x+1][y-1] + boardArray[x-1][y] + boardArray[x+1][y] + boardArray[x-1][y+1] + boardArray[x][y+1] + boardArray[x+1][y+1]
+
+    newState = 0
+    # if state == 1 and (counter == 2 or counter == 3):
+    #     newState = 1
+    # elif state == 1:
+    #     newState = 0
+    # if state == 0 and counter == 3:
+    #     newState = 1
+    # elif state == 0:
+    #     newState = 0
+    if state + counter == 3:
         newState = 1
-    elif state == 1:
-        newState = 0
-    if state == 0 and counter == 3:
-        newState = 1
-    elif state == 0:
-        newState = 0
+    if state + counter == 4:
+        newState = state
 
     return newState
 
@@ -116,17 +142,18 @@ def drawRPentomino(x,y):
     writeState(x+1,y+2,1)
             
 #####################################################################################################################
-setupArray(150,150)#height,width
+setupArray(100,100)#height,width
 setupScreen(5)#cell pixel size
 
-# drawBlinker(20,20)
-drawGlider(20,10)
-drawLightWeightSpaceship(10,20)\
+# drawBlinker(4,4)
+# drawGlider(5,5)
+# drawLightWeightSpaceship(10,180)
 
-drawRPentomino(100,100)
+drawRPentomino(50,50)
 
 simulating = True
-
+pygame.init()
+FPS = clock.get_fps()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -142,6 +169,7 @@ while running:
             wasPressed = 0        
                   
         boardArray = updateBoardArray(boardArray)
+        FPS = (clock.get_fps() + FPS)/2 
     else:###############################################              EDITING              #######################################
         if keypressed[pygame.K_SPACE]:
             if wasPressed == 0:
@@ -156,9 +184,13 @@ while running:
                 wasPressedRight = 1
         else:
             wasPressedRight = 0
+
         
-        
+        print(pygame.time.get_ticks())
+        print(clock.get_fps(), FPS)
+        running = False 
         ###########################################################################################################################
     drawArray()
     pygame.display.update()
-    # clock.tick(fps)    
+    
+    clock.tick()    
