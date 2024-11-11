5bfde004c39718c9db07c53dcdab5fc5eb3cde00
maticjaksic <127771687+maticjaksic@users.noreply.github.com>
2024-11-11 21:55:08 +0100

    Update GameOfLife.py
    draw shape by array

--- GameOfLife.py
+++ GameOfLife.py
@@ -31,24 +31,16 @@ def drawCell(surface,color,left,top,sideLength):
 def drawArray():
     for x in range(arrayHeight):
         for y in range(arrayWidth):
-            if boardArray[x][y] == 1:
-                drawCell(screen,aliveRGB,x*cellSize+1,y*cellSize+1,cellSize-2)
+            if cellSize > 4:
+                if boardArray[x][y] == 1:
+                    drawCell(screen,aliveRGB,x*cellSize+1,y*cellSize+1,cellSize-2)
+                else:
+                    drawCell(screen,deadRGB,x*cellSize+1,y*cellSize+1,cellSize-2)
             else:
-                drawCell(screen,deadRGB,x*cellSize+1,y*cellSize+1,cellSize-2)
-
-# def updateBoardArray(array):
-#     newBoardArray = [ [0]*arrayWidth for i in range(arrayHeight)]
-#     for x in range(arrayWidth):
-#         for y in range(arrayHeight):
-#             if (x == 0 or y >= arrayWidth-1 or y == 0 or x >= arrayHeight-1) == False:
-                
-#                 newBoardArray[x][y] = getState(x,y)
-#     if array == newBoardArray:
-#         global simulating
-#         simulating = False
-#     array = []
-#     array = newBoardArray
-#     return array
+                if boardArray[x][y] == 1:
+                    drawCell(screen,aliveRGB,x*cellSize,y*cellSize,cellSize)
+                else:
+                    drawCell(screen,deadRGB,x*cellSize,y*cellSize,cellSize)
 
 def updateBoardArray(array):
     oldArray = [0]*arrayWidth
@@ -65,43 +57,16 @@ def updateBoardArray(array):
     return array
                 
             
-def getState(x,y):
-   
+def getState(x,y): 
     state = boardArray[x][y]
-
     counter = 0
 
-    # if boardArray[x-1][y-1] == 1:
-    #     counter += 1
-    # if boardArray[x][y-1] == 1:
-    #     counter += 1
-    # if boardArray[x+1][y-1] == 1:
-    #     counter += 1
-    # if boardArray[x-1][y] == 1:
-    #     counter += 1
-    # if boardArray[x+1][y] == 1:
-    #     counter += 1
-    # if boardArray[x-1][y+1] == 1:
-    #     counter += 1
-    # if boardArray[x][y+1] == 1:
-    #     counter += 1
-    # if boardArray[x+1][y+1] == 1:
-    #     counter += 1
-
-    counter = boardArray[x-1][y-1] + boardArray[x][y-1] + boardArray[x+1][y-1] + boardArray[x-1][y] + boardArray[x+1][y] + boardArray[x-1][y+1] + boardArray[x][y+1] + boardArray[x+1][y+1]
-
+    counter = boardArray[x-1][y-1] + boardArray[x][y-1] + boardArray[x+1][y-1] + boardArray[x-1][y] + boardArray[x+1][y] + boardArray[x-1][y+1] + boardArray[x][y+1] + boardArray[x+1][y+1] + state
     newState = 0
-    # if state == 1 and (counter == 2 or counter == 3):
-    #     newState = 1
-    # elif state == 1:
-    #     newState = 0
-    # if state == 0 and counter == 3:
-    #     newState = 1
-    # elif state == 0:
-    #     newState = 0
-    if state + counter == 3:
+    
+    if counter == 3:
         newState = 1
-    if state + counter == 4:
+    if counter == 4:
         newState = state
 
     return newState
@@ -109,6 +74,14 @@ def getState(x,y):
 def writeState(x,y,state):
     boardArray[x][y] = state
 ######################################################################################################################
+def drawShape(x,y,array):
+    for i in range(len(array)):
+        for j in range(len(array[i])):
+            writeState(i+x,j+y,array[i][j])
+
+def drawBlinker(x,y):
+    drawShape(x,y,[[1,1,1]])
+
 def drawBlinker(x,y):
     writeState(x,y,1)
     writeState(x,y+1,1)
@@ -142,14 +115,14 @@ def drawRPentomino(x,y):
     writeState(x+1,y+2,1)
             
 #####################################################################################################################
-setupArray(100,100)#height,width
+setupArray(200,200)#height,width
 setupScreen(5)#cell pixel size
 
-# drawBlinker(4,4)
+# drawBlinker(50,50)
 # drawGlider(5,5)
-# drawLightWeightSpaceship(10,180)
+# drawLightWeightSpaceship(50,50)
 
-drawRPentomino(50,50)
+drawRPentomino(100,100)
 
 simulating = True
 pygame.init()
@@ -184,11 +157,7 @@ while running:
                 wasPressedRight = 1
         else:
             wasPressedRight = 0
-
-        
-        print(pygame.time.get_ticks())
-        print(clock.get_fps(), FPS)
-        running = False 
+ 
         ###########################################################################################################################
     drawArray()
     pygame.display.update()
